829435bcc6ce87325a7ded900653c2691cca3f30
KimGunWoo9595 <medneter@naver.com>
2022-11-05 13:11:48 +0900

    spring data jpa를 이용한 개인프로젝트

---
+++ src/test/java/gunproject/gunshop/GunshopApplicationTests.java
@@ -0,0 +1,13 @@
+package gunproject.gunshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GunshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
