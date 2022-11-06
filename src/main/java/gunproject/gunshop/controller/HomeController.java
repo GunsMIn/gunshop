302edeb3fe625e2d671dd24caed0c6eb8b1e4639
KimGunWoo9595 <medneter@naver.com>
2022-11-06 17:48:44 +0900

    홈 컨트롤러

--- src/main/java/gunproject/gunshop/controller/HomeController.java
+++ src/main/java/gunproject/gunshop/controller/HomeController.java
@@ -1,4 +1,20 @@
 package gunproject.gunshop.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
 public class HomeController {
+
+
+    @RequestMapping("/")
+    public String goHome() {
+        log.info("gunwoo,s shop의 홈 컨트럴러");
+        return "home";
+    }
+
 }
