39419880cedb9dbf8cc5fcaf0b5749fca65d757e
KimGunWoo9595 <medneter@naver.com>
2022-11-14 21:44:42 +0900

    memberRepository

--- src/main/java/gunproject/gunshop/repository/MemberRepository.java
+++ src/main/java/gunproject/gunshop/repository/MemberRepository.java
@@ -4,9 +4,14 @@ import gunproject.gunshop.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
      //service단에서 멤버의 이름 중복체크하려고 만든 repository메소드
      List<Member> findByName(String name);
+
+     //loginId로 회원 찾기
+     Optional<Member> findByLoginId(String loginId);
+
 }
