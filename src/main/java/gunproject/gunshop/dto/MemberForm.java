e5f8888519c739e9b12b5210f72268cd81a22af5
KimGunWoo9595 <medneter@naver.com>
2022-11-14 21:45:43 +0900

    memberDto 필드 추가

--- src/main/java/gunproject/gunshop/dto/MemberForm.java
+++ src/main/java/gunproject/gunshop/dto/MemberForm.java
@@ -11,9 +11,15 @@ import javax.validation.constraints.Size;
 @Getter @Setter
 public class MemberForm {
 
-    @NotEmpty(message = "회원의 이름은 필수입니다.") 
+    @NotEmpty(message = "회원의 이름은 필수입니다.")
     @Size(min=1,max = 10,message="이름은 1~10자 이여야합니다.")
     private String name;
+
+    @NotEmpty
+    private String loginId;
+
+    @NotEmpty
+    private String password;
 
     private String city;
 
