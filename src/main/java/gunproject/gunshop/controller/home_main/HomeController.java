45d42630c7d561cc56ab0ed4a32de55c866badc4
KimGunWoo9595 <medneter@naver.com>
2022-11-14 21:48:42 +0900

    HomeController

---
+++ src/main/java/gunproject/gunshop/controller/home_main/HomeController.java
@@ -0,0 +1,35 @@
+package gunproject.gunshop.controller.home_main;
+
+import gunproject.gunshop.domain.Member;
+import gunproject.gunshop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class HomeController {
+
+    private final MemberService memberService;
+
+    // 회원가입 로그인 홈 화면
+    @RequestMapping("/")
+    public String goHome(@CookieValue(name = "memberId",required = false) Long memberId, Model model) {
+        if (memberId==null) {
+            return "home";
+        }
+        //로그인 성공 시
+        Member member = memberService.findOne(memberId);
+        if (member == null) { // 예전 회원은 디비에 없을 수 도 있다
+            return "home";
+        }
+        
+        model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+}
