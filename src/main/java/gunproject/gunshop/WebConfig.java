6716aa94b4a2f83f6a102547748d102044d55793
KimGunWoo9595 <medneter@naver.com>
2022-11-18 17:56:16 +0900

    인증체크 적용 URI,미적용 URI 설정

---
+++ src/main/java/gunproject/gunshop/WebConfig.java
@@ -0,0 +1,22 @@
+package gunproject.gunshop;
+
+
+import gunproject.gunshop.intercepter.LogInterceptor;
+import gunproject.gunshop.intercepter.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //회원가입 로그인 로그아웃 등등 화면은 로그인사용자 인증체크 제외
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error");
+    }
+}
