f12ea784a372d0d6695f02da6b2604d867b4fa4c
KimGunWoo9595 <medneter@naver.com>
2022-11-18 17:54:58 +0900

    인증 체크 interceptor  홈 컨트롤러

--- src/main/java/gunproject/gunshop/controller/home_main/HomeController.java
+++ src/main/java/gunproject/gunshop/controller/home_main/HomeController.java
@@ -2,12 +2,14 @@ package gunproject.gunshop.controller.home_main;
 
 import gunproject.gunshop.domain.Member;
 import gunproject.gunshop.service.MemberService;
+import gunproject.gunshop.session.SessionConst;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,10 +18,26 @@ public class HomeController {
 
     private final MemberService memberService;
 
-    // 회원가입 로그인 홈 화면
+
     @RequestMapping("/")
-    public String goHome(@CookieValue(name = "memberId",required = false) Long memberId, Model model) {
-        if (memberId==null) {
+    public String home(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,Model model) {
+
+        if (loginMember == null) {
+            return "home";
+        }
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+
+
+
+    /**********************************************  리펙토링 전 쿠키 홈 화면  *******************************************************/
+    // 회원가입 로그인 홈 화면
+    //@RequestMapping("/")
+    public String goHome(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+
+        if (memberId == null) {
             return "home";
         }
         //로그인 성공 시
@@ -27,7 +45,7 @@ public class HomeController {
         if (member == null) { // 예전 회원은 디비에 없을 수 도 있다
             return "home";
         }
-        
+
         model.addAttribute("member", member);
         return "loginHome";
     }
