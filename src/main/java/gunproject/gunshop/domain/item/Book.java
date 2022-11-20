04cb3adf141275657431ba614b7569efbd644ff7
KimGunWoo9595 <medneter@naver.com>
2022-11-20 10:25:04 +0900

    생성자 추사

--- src/main/java/gunproject/gunshop/domain/item/Book.java
+++ src/main/java/gunproject/gunshop/domain/item/Book.java
@@ -1,16 +1,24 @@
 package gunproject.gunshop.domain.item;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
-@Entity @Getter @Setter
+@Entity @Getter @Setter @NoArgsConstructor
 @DiscriminatorValue("book")
 public class Book extends Item {
 
     private String author;
     private String isbn;
+
+
+    public Book(Long id, String name, Integer price, Integer stockQuantity, String author, String isbn, Long fileId) {
+        super(id,name,price,stockQuantity,fileId);
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+
 }
 
