970a4d3477ad0f657694c68f3dded6fe66b2239b
KimGunWoo9595 <medneter@naver.com>
2022-11-20 10:25:26 +0900

    Validation , bulid 추가

--- src/main/java/gunproject/gunshop/dto/BookForm.java
+++ src/main/java/gunproject/gunshop/dto/BookForm.java
@@ -1,13 +1,16 @@
 package gunproject.gunshop.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import gunproject.gunshop.domain.item.Book;
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
-@Getter @Setter
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor @ToString
 public class BookForm {
 
     //수정(update)을 위해 필요함
@@ -19,10 +22,25 @@ public class BookForm {
     @Max(value = 1000000)
     private Integer price;
 
+    @NotNull(message = "상품의 재고를 입력해주세요")
     private Integer stockQuantity;
 
+    @NotEmpty(message = "상품의 저자 입력해주세요")
     private String author;
 
+    @NotEmpty(message = "상품의 일련번호를 입력해주세요")
     private String isbn;
+
+    private Long fileId;
+
+    private MultipartFile file; // 첨부 파일 1건
+
+    private List<MultipartFile> files; // list 첨부파일
+
+    //엔티티로 변환 메소드
+    public Book toEntity() {
+        return new Book(id,name,price,stockQuantity,author,isbn,fileId);
+    }
+
 
 }
