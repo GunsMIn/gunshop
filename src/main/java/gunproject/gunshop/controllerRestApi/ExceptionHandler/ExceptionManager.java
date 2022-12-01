09973fa5b80bd37ec609cc06e3720dd0a5cdac55
KimGunWoo9595 <medneter@naver.com>
2022-12-01 17:18:57 +0900

    ExceptionHandler (IllegalStateMent Error -> 409status)

--- src/main/java/gunproject/gunshop/controllerRestApi/ExceptionHandler/ExceptionManager.java
+++ src/main/java/gunproject/gunshop/controllerRestApi/ExceptionHandler/ExceptionManager.java
@@ -1,4 +1,18 @@
 package gunproject.gunshop.controllerRestApi.ExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+@Slf4j
+@RestControllerAdvice
 public class ExceptionManager {
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ErrorResult> manageIllegalException(IllegalStateException e) {
+        log.info("IllegalStateException 예외핸들러 들어옴");
+        ErrorResult errorResult = new ErrorResult("409 error", "name is duplicated");
+        return new ResponseEntity<>(errorResult, HttpStatus.CONFLICT);
+    }
 }
