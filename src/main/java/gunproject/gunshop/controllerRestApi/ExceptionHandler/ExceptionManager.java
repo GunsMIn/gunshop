bdffb158f38d11b4ca460159367a480da3ddc595
KimGunWoo9595 <medneter@naver.com>
2022-12-01 21:20:29 +0900

    ExceptionHandeler (OrderException) 500->404 status

--- src/main/java/gunproject/gunshop/controllerRestApi/ExceptionHandler/ExceptionManager.java
+++ src/main/java/gunproject/gunshop/controllerRestApi/ExceptionHandler/ExceptionManager.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
@@ -12,7 +13,14 @@ public class ExceptionManager {
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<ErrorResult> manageIllegalException(IllegalStateException e) {
         log.info("IllegalStateException 예외핸들러 들어옴");
-        ErrorResult errorResult = new ErrorResult("409 error", "name is duplicated");
+        ErrorResult errorResult = new ErrorResult("CONFLICT 409 error", "name is duplicated");
         return new ResponseEntity<>(errorResult, HttpStatus.CONFLICT);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(OrderException.class)
+    public ErrorResult manageOrderException(OrderException e) {
+        ErrorResult errorResult = new ErrorResult("NOT_FOUND 404 error", e.getMessage());
+        return errorResult;
     }
 }
