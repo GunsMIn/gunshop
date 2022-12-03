01806165cdf262f8de8ff40c5f96b132c56cec5f
KimGunWoo9595 <medneter@naver.com>
2022-12-03 22:38:24 +0900

    Spring data JPA OrderRepository extends JPArepo and OrderCustionrepo

--- src/main/java/gunproject/gunshop/repository/OrderRepository.java
+++ src/main/java/gunproject/gunshop/repository/OrderRepository.java
@@ -1,112 +1,11 @@
 package gunproject.gunshop.repository;
 
 import gunproject.gunshop.domain.Order;
-import gunproject.gunshop.domain.OrderSearch;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public class OrderRepository { // Spring data jpa를 사용하지않은 repository
-
-    private final EntityManager em;
-
-    public OrderRepository(EntityManager em) {
-        this.em = em;
-    }
-
-    public void save(Order order) {
-        em.persist(order);
-    }
-
-    public Optional<Order> findOne(Long id) {
-        Order order = em.find(Order.class, id);
-        Optional<Order> optionalOrder = Optional.ofNullable(order); // service단에서 orElseThrow로 exceptionHandler 처리를 해줄 것이다
-        return optionalOrder;
-    }
+import gunproject.gunshop.repository.customRepository.OrderCustomRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
-    public List<Order> findAll() {
-        return em.createQuery("select o from Order o join fetch o.member m " +
-                "join fetch o.delivery d")
-                .getResultList();
-    }
-
-    public List<Order> findAllByString(OrderSearch orderSearch) {
-
-        String jpql = "select o from Order o join o.member m";
-        boolean isFirstCondition = true;
-
-
-        if (orderSearch.getOrderStatus() != null) {
-            if (isFirstCondition) {
-                jpql += " where";
-                isFirstCondition = false;
-            } else {
-                jpql += " and";
-            }
-            jpql += " o.status = :status";
-        }
-
-
-        if (StringUtils.hasText(orderSearch.getMemberName())) {
-            if (isFirstCondition) {
-                jpql += " where";
-                isFirstCondition = false;
-            } else {
-                jpql += " and";
-            }
-            jpql += " m.name like :name";
-        }
-
-        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
-                .setMaxResults(1000);
-
-        if (orderSearch.getOrderStatus() != null) {
-            query = query.setParameter("status", orderSearch.getOrderStatus());
-        }
-        if (StringUtils.hasText(orderSearch.getMemberName())) {
-            query = query.setParameter("name", orderSearch.getMemberName());
-        }
-
-        return query.getResultList();
-    }
-
-    /**
-     * JPA Criteria
-     */
-    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
-        Root<Order> o = cq.from(Order.class);
-        Join<Object, Object> m = o.join("member", JoinType.INNER);
-
-        List<Predicate> criteria = new ArrayList<>();
-
-
-        if (orderSearch.getOrderStatus() != null) {
-            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
-            criteria.add(status);
-        }
-
-        if (StringUtils.hasText(orderSearch.getMemberName())) {
-            Predicate name =
-                    cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
-            criteria.add(name);
-        }
-
-        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
-        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
-        return query.getResultList();
-    }
+public interface OrderRepository extends JpaRepository<Order,Long>, OrderCustomRepository { // Spring data jpa를 사용하지않은 repository
 
 }
 
