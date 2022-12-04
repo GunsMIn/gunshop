1d25e042f2061becaa6ce5e138177646d0654ec0
KimGunWoo9595 <medneter@naver.com>
2022-12-04 16:56:10 +0900

    1:다 / 다:1 관계에서의 조회기능(Batch_fectch_size)

--- src/main/java/gunproject/gunshop/repository/OrderRepository.java
+++ src/main/java/gunproject/gunshop/repository/OrderRepository.java
@@ -2,10 +2,19 @@ package gunproject.gunshop.repository;
 
 import gunproject.gunshop.domain.Order;
 import gunproject.gunshop.repository.customRepository.OrderCustomRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface OrderRepository extends JpaRepository<Order,Long>, OrderCustomRepository { // Spring data jpa를 사용하지않은 repository
+import java.util.List;
 
+//spring data JPA외에 사용자 정의 Repository까지 상속 받음
+public interface OrderRepository extends JpaRepository<Order,Long>, OrderCustomRepository {
+
+    //xxxToOne관계는 fetch join xxxToMany관계는 Batchsize이용
+    @EntityGraph(attributePaths={"member","delivery"})
+    Page<Order> findAll(Pageable pageable);
 }
 
