ddf9baed254c25889972f5bb137c73d54cf336e3
KimGunWoo9595 <medneter@naver.com>
2022-12-05 09:03:20 +0900

    BaseEntity(CreatedeDate,UpdateDate)

---
+++ src/main/java/gunproject/gunshop/domain/BaseEntity.java
@@ -0,0 +1,26 @@
+package gunproject.gunshop.domain;
+
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+//이벤트로 동작한다.
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
